08c16cefc21d95fbc88e0bead2b89b8ba2e6b0fa
Joseph Gitau <joseph.gitau@canter.fi>
2021-01-24 13:51:15 +0200

    connect to remote mongodb cluster

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.use(express.json());
 
@@ -22,4 +24,4 @@ app.get('/api/products', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV
-app.listen(PORT, () => console.log(`Server running in ${NODE_ENV} mode on port ${PORT}...`))
+app.listen(PORT, () => console.log(`Server running in ${NODE_ENV} mode on port ${PORT}...`));
