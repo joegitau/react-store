9e5f4354b897a9e8a02f9e3f07cefbf6540bddc6
Joseph Gitau <joseph.gitau@canter.fi>
2021-01-26 22:54:21 +0200

    integrate redux to manage state

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,60 @@ router.get("/", async (req, res) => {
     const users = await User.find({});
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).send("Users not found.");
+    res.status(400).json({
+      err: error.message,
+      msg: "Users not found."
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({
+      err: error.message,
+      msg: "Users with given ID not found."
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({
+      err: error.message,
+      msg: "User not created."
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({
+      err: error.message,
+      msg: "Users with given ID not updated."
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({
+      err: error.message,
+      msg: "Users with given ID not deleted."
+    });
   }
 });
 
